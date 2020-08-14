4628bc2bb51de7e00f69653e73fe2ff3f19f35da
hhr <hr650jsjx@163.com>
2020-08-14 14:26:50 +0800

    多线程初始化

---
+++ javase/code/07MultithreadAndThreadPool/src/com/thread/base/other/ThreadDemo01.java
@@ -0,0 +1,9 @@
+package com.thread.base.other;
+
+public class ThreadDemo01 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 1000000000; i++) {
+            System.out.println("hello");
+        }
+    }
+}
