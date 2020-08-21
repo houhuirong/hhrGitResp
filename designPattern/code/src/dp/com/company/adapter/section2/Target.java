21547324acf843166ebbf223bab412b1f23c39c0
hhr <hr650jsjx@163.com>
2020-08-21 17:11:44 +0800

    设计模式之禅初始化

---
+++ designPattern/code/src/dp/com/company/adapter/section2/Target.java
@@ -0,0 +1,12 @@
+package com.company.adapter.section2;
+
+/**
+ * @author cbf4Life cbf4life@126.com <br/>
+ *         I'm glad to share my knowledge with you all. <br/>
+ *         目标角色
+ */
+public interface Target {
+
+	// 目标角色有自己的方法
+	public void request();
+}
