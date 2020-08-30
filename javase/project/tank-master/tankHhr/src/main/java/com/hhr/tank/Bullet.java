d338dbe3aa96f970d1303d7a6c4c9da3aba147bc
hhr <hr650jsjx@163.com>
2020-08-30 11:11:34 +0800

    子弹换成图片

--- javase/project/tank-master/tankHhr/src/main/java/com/hhr/tank/Bullet.java
+++ javase/project/tank-master/tankHhr/src/main/java/com/hhr/tank/Bullet.java
@@ -27,10 +27,20 @@ public class Bullet {
         if(!live){
             tf.bulletS.remove(this);
         }
-        Color c=g.getColor();
-        g.setColor(Color.RED);
-        g.fillRect(x,y,WIDTH,HEIGHT);
-        g.setColor(c);
+        switch (dir){
+            case LEFT:
+                g.drawImage(ResourceMgr.bulletL,x,y,null);
+                break;
+            case RIGHT:
+                g.drawImage(ResourceMgr.bulletR,x,y,null);
+                break;
+            case UP:
+                g.drawImage(ResourceMgr.bulletU,x,y,null);
+                break;
+            case DOWN:
+                g.drawImage(ResourceMgr.bulletD,x,y,null);
+                break;
+        }
         move();
     }
 
