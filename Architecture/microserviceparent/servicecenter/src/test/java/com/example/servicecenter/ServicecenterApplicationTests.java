165e223a557a52d79c8d798f61702a46bdca390b
hhr <hr650jsjx@163.com>
2020-10-15 13:50:32 +0800

    搭建两套eureka初始化环境

---
+++ Architecture/microserviceparent/servicecenter/src/test/java/com/example/servicecenter/ServicecenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servicecenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicecenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Architecture/microserviceparent/servicecenter2/src/test/java/com/example/servicecenter2/Servicecenter2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servicecenter2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Servicecenter2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
